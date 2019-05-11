7e18639065afc9067455d0e1ba9dbafd27b558f7
edz <347511109@qq.com>
2019-05-11 20:07:36 +0800

    no message

--- WXMRealmManager.h
+++ WXMRealmManager.h
@@ -14,26 +14,22 @@
 @interface WXMRealmManager : NSObject
 
 + (WXMRealmManager *)sharedInstance;
-
-/**  */
 //- (void)judgeRealmCache;
 
-#pragma mark _____________________________________________ 增 + 改
-- (BOOL)saveRLMObjectWithObj:(RLMObject *)obj;                              /** 增加单个 */
-- (NSInteger)saveRLMObjectsWithArray:(NSArray<RLMObject *> *)array;         /** 增加多个 */
-- (NSInteger)saveRLMObjectsSingletonWithArray:(NSArray<RLMObject *> *)array;/** 先删除(副键)再添加 */
+#pragma mark _____________________________________________ 增+改
+- (BOOL)saveRLMObjectWithObj:(RLMObject *)obj;                              /** 主键增加单个 */
+- (NSInteger)saveRLMObjectsWithArray:(NSArray<RLMObject *> *)array;         /** 副键增加多个 */
+- (NSInteger)saveRLMObjectsSingletonWithArray:(NSArray<RLMObject *> *)array;/** 副键先删除再加多个 */
 
 #pragma mark _____________________________________________ 查
-/** 通过主键获取单个缓存对象(主键唯一) */
-- (id)objWithPrimaryKeyValue:(id)primaryKeyValue class:(Class)aClass;
+- (id)objWithPrimaryKeyValue:(id)primaryKeyValue class:(Class)aClass;/** 主键查询 */
 - (NSArray <RLMObject *>*)objWithViceKeyValue:(id)viceKeyValue class:(Class)aClass;/** 副键查询 */
 - (NSArray <RLMObject *>*)objWithViceKey:(id)viceKey
                             viceKeyValue:(id)viceKeyValue
                                    class:(Class)aClass;
 
 #pragma mark _____________________________________________ 删除
-/** 通过主键删除 */
-- (BOOL)removeRLMObjectWithObj:(RLMObject *)obj;
+- (BOOL)removeRLMObjectWithObj:(RLMObject *)obj; /** 主键删除 */
 - (BOOL)removeRLMObjectWithPrimaryKeyValue:(id)primaryKeyValue class:(Class)aClass;
 - (NSInteger)removeRLMObjectWithViceKeyValue:(id)viceKeyValue class:(Class)aClass;/** 副键删除 */
 - (NSInteger)removeRLMObjectWithViceKey:(id)viceKey
